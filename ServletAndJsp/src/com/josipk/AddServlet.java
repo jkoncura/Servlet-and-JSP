df7cad65ab1f39939b6ae1db16a379390c3fbe1a
Josip Koncurat <jkoncura@gmail.com>
2019-07-05 10:00:07 +0200

    Calling a Servlet from a Servlet

--- ServletAndJsp/src/com/josipk/AddServlet.java
+++ ServletAndJsp/src/com/josipk/AddServlet.java
@@ -2,21 +2,31 @@ package com.josipk;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
 		
-		PrintWriter out = res.getWriter();
+		PrintWriter out = resp.getWriter();
 		
 		out.println("result is: " + k);
-	} 
+		
+		//To call another servlet we can use a Request Dispatcher or Redirect
+		// Session management
+		req.setAttribute("k", k);
+		
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("sq");
+		requestDispatcher.forward(req, resp);
+	}
 
 }
---
+++ ServletAndJsp/src/com/josipk/SquareServlet.java
@@ -0,0 +1,24 @@
+package com.josipk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		int k = (int) req.getAttribute("k");
+		
+		k = k*k;
+		
+		PrintWriter out = resp.getWriter();
+		out.println("Result is: " + k);
+	}
+
+}
