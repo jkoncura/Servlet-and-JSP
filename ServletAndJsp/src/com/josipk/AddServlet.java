c76ba001e52f768fa4e53793e08e6d19e482f926
Josip Koncurat <jkoncura@gmail.com>
2019-07-05 14:05:40 +0200

    Adding JSP

--- ServletAndJsp/src/com/josipk/AddServlet.java
+++ ServletAndJsp/src/com/josipk/AddServlet.java
@@ -2,14 +2,11 @@ package com.josipk;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/add")
 public class AddServlet extends HttpServlet {
@@ -17,15 +14,5 @@ public class AddServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
-		int k = i + j;
-		
-		Cookie cookie = new Cookie("k", k + "");
-		resp.addCookie(cookie);
-		
-		resp.sendRedirect("sq");
-		
 	}
-
 }
